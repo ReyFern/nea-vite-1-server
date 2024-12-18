c081113db712bb163dd8e5ae72965f2befc77e40
Davian <rehan.davian@gmail.com>
2024-12-18 17:54:40 +0000

    Sends json data to /api route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from our server!');
+app.get('/api', (req, res) => {
+    res.json({users: ["userOne", "userTwo", "userThree"]});
 });
 
 app.listen(5000, () => {
