263f8a23ab9c89966f14acd0f49fab69e27bf02f
Davian <rehan.davian@gmail.com>
2024-12-18 22:31:34 +0000

    Installed cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api', (req, res) => {
-    res.json({users: ["userOne", "userTwo", "userThree"]});
+    res.json({"users": ["userOne", "userTwo", "userThree"]});
 });
 
 app.listen(5000, () => {
